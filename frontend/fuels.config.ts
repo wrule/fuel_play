273d6140ceba1814885f094d8192e238cdb1c3bd
wrule <1982775886@qq.com>
2024-03-23 20:58:01 +0800

    构建生成类型

---
+++ frontend/fuels.config.ts
@@ -0,0 +1,12 @@
+import { createConfig } from 'fuels';
+
+export default createConfig({
+  contracts: [
+  ],
+  output: './src/sway-api',
+});
+
+/**
+ * Check the docs:
+ * https://fuellabs.github.io/fuels-ts/guide/cli/config-file
+ */
